77c326c684dd0bd1a23c91a9c9cb9076b926c3ed
skaysrei <alerocco90@gmail.com>
2023-04-03 02:24:26 +0200

    groundstone

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import ProfileWidget from './sidebar/ProfileWidget';
+
+import './Sidebar.scss'
+
+export default function Sidebar() {
+    return (
+    <div class="">
+        home
+        <ProfileWidget/>
+    </div>
+    );
+};
---
+++ src/components/sidebar/ProfileWidget.tsx
@@ -0,0 +1,11 @@
+import './ProfileWidget.scss';
+
+export default function ProfileWidget() {
+    return (
+        <div class="profile-widget">
+            <div class="center">
+                <img src="/mock_user/icon.jpg"/>
+            </div>
+        </div>
+    );
+};
